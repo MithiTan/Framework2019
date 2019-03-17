cec866bbfebef4472d2fd61cbb664f07a5ac7c54
MithiTan <jannatul486410@gmail.com>
2019-03-17 12:53:48 -0400

    Update LoginFacebook.java

--- src/test/facebook/LoginFacebook.java
+++ src/test/facebook/LoginFacebook.java
@@ -7,15 +7,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginFacebook {
-
+//tell the code where is your driver locators
 	public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\mithitan\\Documents\\GitHub\\Framework2019\\drivers\\chromedriver.exe");
+	//intit the chrome driver
 	WebDriver driver=new ChromeDriver();
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("sonia");
-	driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("sonia");
+	//locators
+	String Fnloc="/input[@name='firstname']";
+	
+	
+	
+	
+	
+	
+	
+	
+	//Enter first name 
+	driver.findElement(By.xpath("//input[@name='firstname']")).sendKeys("Fname");
+	//Enter last name
+	driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Lname");
 
 	}
 
