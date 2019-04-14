7eb8d7000b208ef51cd273dcc439eaa1a4da2fa0
MithiTan <jannatul486410@gmail.com>
2019-04-14 12:41:30 -0400

    commit JIRA-324

---
+++ src/locators/Facebooklocators.java
@@ -0,0 +1,26 @@
+package locators;
+
+public class Facebooklocators {
+	public	String email = "//*[@id='u_0_h']";
+
+
+	 
+	public	String pass = "//input[@type='password']";
+
+
+ 
+
+
+
+ 
+	public String fnLoc = "//input[@name='firstname']";
+
+
+ 
+	public String lnLoc = "//input[@name='lastname']";
+
+
+ 
+
+
+}
---
+++ src/values/FacebookValues.java
@@ -0,0 +1,15 @@
+package values;
+
+public class FacebookValues {
+	
+	String fnVal = "fnTest";
+
+	String lnVal = "lnTest";
+	
+	String emailVal = "taltekc@gmail.com";
+
+	String passVal = "test.2324";
+
+	
+
+}
